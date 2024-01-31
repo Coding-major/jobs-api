a43a13a2d4c976e778307e1dc4dbcc7542516d18
coding-major <shehumustapham@gmail.com>
2024-01-31 20:05:43 +0100

    removed the if statement used to check the instaceof custom error in the errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,16 +1,16 @@
 const {StatusCodes} = require("http-status-codes")
-const {customError} = require("../errors")
+//const {customError} = require("../errors")
 
 
 const errorHandler = (err, req, res, next) => {
-    if (err instanceof customError) {
-        return res.status(err.statusCode).json({msg: err.message})
-    }
-
-    // if (err.code && err.code === 11000) {
-    //     return res.status(StatusCodes.BAD_REQUEST).json({err: "mmmmmmmmmm"})
+    // if (err instanceof customError) {
+    //     return res.status(err.statusCode).json({msg: err.message})
     // }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+
+    if (err.code && err.code === 11000) {
+        return res.status(StatusCodes.BAD_REQUEST).json({msg: "user with that email already exist"})
+    }
+    return res.status(err.statusCode).json({err: err.message})
 }
 
 module.exports = errorHandler
